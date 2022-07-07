41c8cf8cdf4803293b33df811ca657aea1a59ed0
Marysabel Morales <marysabel.m31@gmail.com>
2022-07-07 19:03:43 +0000

    style changes

--- flights.py
+++ flights.py
@@ -13,8 +13,7 @@ def auth_amadeus():
     AUTH_URL = 'https://test.api.amadeus.com/v1/security/oauth2/token'
     auth_response = requests.post(AUTH_URL, {'grant_type': 'client_credentials',
                                              'client_id': os.environ.get('AMADEUS_CLIENT_ID'),
-                                             'client_secret': os.environ.get('AMADEUS_CLIENT_SECRET')
-                                            }
+                                             'client_secret': os.environ.get('AMADEUS_CLIENT_SECRET')}
                                   )
     auth_response_data = auth_response.json()
     access_token = auth_response_data['access_token']
@@ -83,9 +82,9 @@ def get_flights_request(headers, departure_airport_iata, arrival_airport_iata):
             "travelers": [{"id": "1", "travelerType": "ADULT"}],
             "sources": ["GDS"]
           }
-  auth_response = requests.post(BASE_URL, headers=headers, json=data)
-  auth_response_data = auth_response.json()
-  return(auth_response_data)
+    auth_response = requests.post(BASE_URL, headers=headers, json=data)
+    auth_response_data = auth_response.json()
+    return(auth_response_data)
 
 # make list of route iata's for display then prints it for the user (FUNCTION)
 
