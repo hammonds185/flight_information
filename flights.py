9897445472ed9fe40bcd0e4b9bc07a73fac5e9ec
Marysabel Morales <marysabel.m31@gmail.com>
2022-07-08 06:45:04 +0000

    same code as before

--- flights.py
+++ flights.py
@@ -2,19 +2,24 @@ import requests
 import os
 import pandas as pd
 import sqlalchemy as db
+import sys
+import datetime
 
 
 API_KEY = os.environ.get('AVIATIONSTACK_API_KEY')
 
-# Authorization Functio
+# Authorization Function
 
 
 def auth_amadeus():
     AUTH_URL = 'https://test.api.amadeus.com/v1/security/oauth2/token'
     auth_response = requests.post(AUTH_URL, {
-                                             'grant_type': 'client_credentials',
-                                             'client_id': os.environ.get('AMADEUS_CLIENT_ID'),
-                                             'client_secret': os.environ.get('AMADEUS_CLIENT_SECRET')}
+                                            'grant_type': 'client_credentials'
+                                             , 'client_id':
+                                             os.environ.get('AMADEUS_CLIENT_ID'
+                                             ),'client_secret':
+                                             os.environ.get(
+                                                 'AMADEUS_CLIENT_SECRET')}
                                   )
     auth_response_data = auth_response.json()
     access_token = auth_response_data['access_token']
@@ -22,13 +27,14 @@ def auth_amadeus():
                'X-HTTP-Method-Override': 'GET'}
     return headers
 
-# FUNCTION GET_REQUEST, RETURN JSON RESPONSE
+# Function for getting airports
 
 
 def get_request(headers, city):
     params = {'subType': ['AIRPORT'],
               'keyword': str(city)}
-    BASE_URL = 'https://test.api.amadeus.com/v1/reference-data/locations?subType=AIRPORT&keyword=' + city + '&view=LIGHT'
+    BASE_URL = 'https://test.api.amadeus.com/v1/reference-data/locations?'\
+    'subType=AIRPORT&keyword=' + city + '&view=LIGHT'
     r = requests.get(BASE_URL, headers=headers)
     return(r.json())
 
@@ -66,16 +72,29 @@ def print_arrive_airports(arrive_iata_list, arrive_name_list):
         arrive_name_list.append(name)
         numbering += 1
 
+
+# Validate Date
+
+
+def validate(date_text):
+    try:
+        datetime.datetime.strptime(date_text, '%Y-%m-%d')
+    except ValueError:
+        print("Incorrect data format, should be YYYY-MM-DD. Please Start Over")
+        sys.exit(1)
+
+
 # POST request for flights FUNCTION
 
 
-def get_flights_request(headers, departure_airport_iata, arrival_airport_iata):
-    BASE_URL = 'https://test.api.amadeus.com/v1/shopping/availability/flight-availabilities'
+def get_flights_request(headers, departure_airport_iata, arrival_airport_iata, date):
+    BASE_URL = 'https://test.api.amadeus.com/v1/shopping/availability/'\
+    'flight-availabilities'
     data = {
             "originDestinations": [{"id": "1",
-                                    "originLocationCode": "CVG",
-                                    "destinationLocationCode": "LAX",
-                                    "departureDateTime": {"date": "2022-07-08",
+                                    "originLocationCode": departure_airport_iata,
+                                    "destinationLocationCode": arrival_airport_iata,
+                                    "departureDateTime": {"date": date,
                                                           "time": "21:15:00"}}],
             "travelers": [{"id": "1", "travelerType": "ADULT"}],
             "sources": ["GDS"]}
@@ -83,12 +102,17 @@ def get_flights_request(headers, departure_airport_iata, arrival_airport_iata):
     auth_response_data = auth_response.json()
     return(auth_response_data)
 
-# make list of route iata's for display then prints it for the user (FUNCTION)
+# Function: make list of route iata's for display then prints it for the user
 
 
 def print_routes(flights_response, flights_list):
     # parse
-    available_routes = flights_response['data']
+    try:
+        available_routes = flights_response['data']
+    except KeyError:
+        print("There are no available flights. Please start again.")
+        sys.exit()
+
     # create list of options
     count = 1
     for route in available_routes:
@@ -101,12 +125,12 @@ def print_routes(flights_response, flights_list):
             arrival_iata = segment['arrival']["iataCode"]
             segment_name = departure_iata + " -> " + arrival_iata
             segment_dict[segment_name] = [departure_iata, segment['departure']["at"], arrival_iata, segment['arrival']["at"]]
-            # print(flights_list)
-            print(segment_name + "\n depart at: " + flights_list[count - 1][str(count)][segment_name][1] + " -> arrive at: " + segment['arrival']["at"])
+            print(segment_name + "\n\tdepart on: " + flights_list[count - 1][str(count)][segment_name][1][:10] + " Time: " + flights_list[count - 1][str(count)][segment_name][1][11:])
+            print("\tarrive on: " + segment['arrival']["at"][:10] + " Time: " + segment['arrival']["at"][11:])
         count += 1
 
 
-print("Enter departure city: ")
+print("Enter departure city: ", end='')
 # change this to user input
 departure_city = input()
 head = auth_amadeus()
@@ -123,20 +147,18 @@ depart_name_list = []
 print_depart_airports(depart_iata_list, depart_name_list)
 
 # get the choice
-departure_airport_choice = input()
+departure_airport_choice = input("Choice: ")
 departure_airport_iata = ''
 departure_airport_name = ''
 try:
     departure_airport_iata = depart_iata_list[int(departure_airport_choice) - 1]
     departure_airport_name = depart_name_list[int(departure_airport_choice) - 1]
-except:
-    print("Your choice is invalid")
-
-# prints the iata to confirm!!!!!
-print(departure_airport_iata)
+except IndexError:
+    print("Your choice is invalid. Please Start Over.")
+    sys.exit()
 
 # now we must get the destination airport
-print("Enter a destination city: ")
+print("Enter a destination city: ", end='')
 
 # change this to user input
 arrival_city = input()
@@ -151,82 +173,59 @@ arrive_name_list = []
 print_arrive_airports(arrive_iata_list, arrive_name_list)
 
 # get the choice
-arrival_airport_choice = input()
+arrival_airport_choice = input("Choice: ")
 arrival_airport_iata = ''
 arrival_airport_name = ''
 try:
     arrival_airport_iata = arrive_iata_list[int(arrival_airport_choice) - 1]
     arrival_airport_name = arrive_name_list[int(arrival_airport_choice) - 1]
-except:
-    print("Your choice is invalid")
+except IndexError:
+    print("Your choice is invalid. Please Start Over")
+    sys.exit()
 
-# prints the iata to confirm!!!!!
-print(arrival_airport_iata)
-
-# call function for POST request to get list of flights
-
+# get a date from the user
+print("Enter your earliest date (yyyy-mm-dd): ", end='')
+date = input()
+validate(date)
 
 # variables to be used outside of the function
 flights_list = []
 
-# POST request
-flights_response = get_flights_request(head, departure_airport_iata, arrival_airport_iata)
-# print(flights_response)
+# call function for POST request to get list of flights
+flights_response = get_flights_request(head, departure_airport_iata, arrival_airport_iata, date)
 
 # display list of available flights
-print("here are flights from " + departure_city + " to " + arrival_city + ": ")
+print("Here are flights from " + departure_city + " to " + arrival_city + ": ")
 print_routes(flights_response, flights_list)
 
 # Choose a flight
-print("Choose a flight: ")
+print("Choose a flight (type the number): ", end='')
 flight_opt_num = input()
 
-# display information
-# Name of airports
-# departure_airport_name
-# arrival_airport_name
-# Dearture City
-# departure_city
-# Arrival City
-# arrival_city
 # Overall route
 overall_route = departure_airport_iata + " -> " + arrival_airport_iata
-print(overall_route)
-# Detailed route
-# the keys in flights_list[int(flight_opt_num) - 1][flight_opt_num]
-# Day of First Flight
-flight_day = list(flights_list[int(flight_opt_num) - 1][flight_opt_num].values())[0][1][:10]
-# Time of first departure
+# Flight Days vars for weather
+try:
+    route_list = list(flights_list[int(flight_opt_num) - 1][flight_opt_num].values())
+except IndexError:
+    print("Your Selection was invalid. Please Start Over")
+    sys.exit()
+start_flight_day = route_list[0][1][:10]
+end_flight_day = route_list[len(route_list) - 1][1][:10]
+# Time of first departure for math
 time_of_departure = list(flights_list[int(flight_opt_num) - 1][flight_opt_num].values())[0][1][11:]
-# Time of departures
-# Time of arrivals
-print(flights_list[int(flight_opt_num) - 1])
 
-# initialize variables
-name = ''
-detailed_name = ''
-iataCode = ''
-iata_list = []
-# numbering = 1
+# name of airports and overall route
+print("Your Flight: " + departure_airport_name + " -> " + arrival_airport_name + "(" + overall_route + ")")
+# departure and arrival city
+print("Dearture City:  " + departure_city + "\nArrival City: " + arrival_city)
+# day and time for departure
+print("Departure Day and Time: " + start_flight_day + " at " + time_of_departure)
+# connections
+print("Flight Connections: ")
+for key in flights_list[int(flight_opt_num) - 1][flight_opt_num]:
+    print(f'\t{key}')
 
-# Print list of airports for user and store info
-# start here
-# for airport in arrival_airports['data']:
-#   name = airport['name']
-#   detailed_name = airport['detailedName']
-#   iataCode = airport['iataCode']
-#   #instead of printing the info we must use the iatas as parameters for aviationstack
-#   # I printed name to see what airport is going into the aviationstack request
-#   print(name)
-#   # the request
-#   available_flights = get_flights_request(API_KEY, departure_airport_iata, iataCode)
-#   # parsing data to get each fight date, departure place, and arrival place
-#   data = available_flights['data']
-#   for flight in data:
-#     print(flight['flight_date'])
-#     print(flight['departure']['airport'])
-#     print(flight['arrival']['airport'])
-#     print("\n")
 
 # Weather code
 
@@ -270,11 +269,8 @@ departure_forecast_dict["temp"] = departure_forecast[0:6]
 departure_forecast_dict["air_condition"] = departure_forecast[6:12]
 departure_forecast_dict["other_conditions"] = departure_forecast[12:18]
 
-
-# Variables for arrival weather 
+# Variables for arrival weather
 arrival_forecast_data = get_forecast(arrival_city)
-
-
 arrival_forecast_dict = {"temp": [], "air_condition": [], "other_conditions": []}
 arrival_forecast = []
 day2_forecast_info = departure_forecast_data["forecast"]["forecastday"][0]
@@ -301,8 +297,8 @@ def create_database(forecast_info_dict):
 
     # creating a database from dataframe
     engine = db.create_engine('sqlite:///weather_forcast.db')
-    forecast_df.to_sql('forecast_info_dict', 
-                       con=engine, 
+    forecast_df.to_sql('forecast_info_dict',
+                       con=engine,
                        if_exists='replace',
                        index=False)
     query_result = engine.execute("SELECT * FROM forecast_info_dict;").fetchall()
